cf49e2512b2474a86742b3c0e10b5ca848a0ce99
je6bmq <je6bmq@gmail.com>
2018-02-04 16:55:37 +0900

    implelemented: the function which loading configuration parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
+
+use std::fs::File;
+
+#[derive(Debug, Deserialize)]
+struct Configuration {
+    mac_addr: String,
+    consumer_key: String,
+    consumer_secret: String,
+    access_token_key: String,
+    access_token_secret: String,
+}
+
+impl Configuration {
+    fn new(file_name: &str) -> Self {
+        let file = File::open(file_name).expect("error occured when loading configuration...");
+        serde_yaml::from_reader(file).expect("error occuered when parsing configuration yaml...")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
